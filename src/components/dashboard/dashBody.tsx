8ad9c7a3fb0f659dad24646dce8f49466f89a497
Harrison <harrisonfemi@yahoo.com>
2026-03-13 09:54:38 +0100

    update:scrollbar on DashSideBar

--- src/components/dashboard/dashBody.tsx
+++ src/components/dashboard/dashBody.tsx
@@ -3,9 +3,14 @@ import { DashHome } from "./dashHome"
 
 export const DashBody = () => {
     return(
-        <div className="w-full pl-[10px] pt-[10px] pb-[10px] pr-[20px] grid grid-cols-[14%_86%] bg-gray-200 gap-3">
-            <DashSideBar />
-            <DashHome />
+        <div className="w-full h-screen flex bg-gray-200">
+            <div className="w-[16%] fixed h-screen overflow-y-auto pl-[10px] pt-[10px] pb-[10px] pr-[10px]">
+                <DashSideBar />
+            </div>
+            <div className="w-[16%]"></div>
+            <div className="flex-1 overflow-y-auto pt-[10px] pb-[10px] pr-[10px] pl-[10px]">
+                <DashHome />
+            </div>
         </div>
     )
 }
