6c02f04f5ede63055f3aa52ed8c026d2441b40c4
Harrison <harrisonfemi@yahoo.com>
2026-03-13 09:59:04 +0100

    update:dashboard width adjustments

--- src/components/dashboard/dashBody.tsx
+++ src/components/dashboard/dashBody.tsx
@@ -4,10 +4,10 @@ import { DashHome } from "./dashHome"
 export const DashBody = () => {
     return(
         <div className="w-full h-screen flex bg-gray-200">
-            <div className="w-[16%] fixed h-screen overflow-y-auto pl-[10px] pt-[10px] pb-[10px] pr-[10px]">
+            <div className="w-[15%] fixed h-screen overflow-y-auto pl-[10px] pt-[10px] pb-[10px] pr-[10px]">
                 <DashSideBar />
             </div>
-            <div className="w-[16%]"></div>
+            <div className="w-[15%]"></div>
             <div className="flex-1 overflow-y-auto pt-[10px] pb-[10px] pr-[10px] pl-[10px]">
                 <DashHome />
             </div>
